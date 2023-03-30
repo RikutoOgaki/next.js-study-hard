457276c11420a7aae0260f6f891838f10882ba37
Rikuto <orikutobasuke@gmail.com>
2023-03-30 15:57:32 +0900

    next.js-study

---
+++ src/pages/samples/07/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import { useState } from 'react'
+
+export default function Sample07() {
+    return (
+        <>
+            <Box>
+
+            </Box>
+        </>
+    )
+}
