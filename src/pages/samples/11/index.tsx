e80a77d6a46c55aa5346fac8f35602755b41c8cd
Rikuto <orikutobasuke@gmail.com>
2023-04-06 14:24:21 +0900

    next.js-study

---
+++ src/pages/samples/11/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Flex, Input, Text } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+
+export default function Sample11() {
+    return (
+        <>
+            <Box></Box>
+        </>
+    )
+}
