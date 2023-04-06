eea38878762fa5bcb7db9470d9af9b9242398305
Rikuto <orikutobasuke@gmail.com>
2023-04-06 16:20:26 +0900

    next.js-study

--- src/pages/samples/11/index.tsx
+++ src/pages/samples/11/index.tsx
@@ -1,12 +1,24 @@
 import { Box, Flex, Input, Text } from '@chakra-ui/react'
-import { motion } from 'framer-motion'
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion'
+import { useEffect } from 'react'
 
 export default function Sample11() {
+
+    const count = useMotionValue(0)
+    const rounded = useTransform(count, Math.round)
+
+    useEffect(() => {
+        const animation = animate(count, 100, { duration: 10 })
+        return animation.stop
+    }, [])
+
     return (
         <>
-            <Box>
-
-            </Box>
+            <Flex w={'100%'} h={'100vh'} justifyContent={'center'} alignItems={'center'} bg={'aqua'} fontSize={'15rem'} color={'#FFF'}>
+                <motion.h1>
+                    {rounded}
+                </motion.h1>
+            </Flex>
         </>
     )
 }
---
+++ src/pages/samples/12/index.tsx
@@ -0,0 +1,61 @@
+import { Box, Flex, Text, Input } from '@chakra-ui/react'
+import { motion, Variants } from 'framer-motion'
+import './index.module.css'
+
+interface Props {
+    moji: string,
+    hueA: number,
+    hueB: number
+}
+
+const cardVariants: Variants = {
+    offscreen: {
+        y: 300
+    },
+    onscreen: {
+        y: 50,
+        rotate: -10,
+        transition: {
+            type: 'spring',
+            bounce: .4,
+            duration: .8
+        }
+    }
+}
+
+const hue = (h: number) => `hsl(${h},100%,50%)`
+
+function Card({ moji, hueA, hueB }: Props) {
+    const background = `liner-gradient(306deg,${hue(hueA)},${hue(hueB)})`
+
+    return (
+        <motion.div
+            className='cardbox'
+            initial='offscreen'
+            whileInView='onscreen'
+            viewport={{ once: true, amount: .8 }}
+        >
+            <div className='splash' style={{ background }} />
+            <motion.div className='card' variants={cardVariants}>
+                {moji}
+            </motion.div>
+        </motion.div>
+    )
+}
+
+const food: [string, number, number][] = [
+    ["🍜", 340, 10],
+    ["🍛", 20, 40],
+    ["🍋", 60, 90],
+    ["🍐", 80, 120],
+    ["🍏", 100, 140],
+    ["🫐", 205, 245],
+    ["🍆", 260, 290],
+    ["🍇", 290, 320]
+]
+
+export default function Sample12() {
+    return food.map(([moji, hueA, hueB]) => (
+        <Card moji={moji} hueA={hueA} hueB={hueB} />
+    ))
+}
