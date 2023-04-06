770302127ef9103460ea9906358766edb6256a4b
Rikuto <orikutobasuke@gmail.com>
2023-04-06 12:00:46 +0900

    next.js-study

---
+++ src/pages/samples/09/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Flex, Input, Text } from '@chakra-ui/react'
+
+export default function Sample09() {
+    return (
+        <>
+            <Box>
+
+            </Box>
+        </>
+    )
+}
