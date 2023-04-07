16abf68163f543f586316567be119a1698ea6cb1
Rikuto <orikutobasuke@gmail.com>
2023-04-07 11:13:33 +0900

    next.js-study

--- src/pages/samples/14/components/Header.tsx
+++ src/pages/samples/14/components/Header.tsx
@@ -1,9 +1,40 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import {
+    Box,
+    Flex,
+    Text,
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList
+} from '@chakra-ui/react'
+import Link from 'next/link'
 
 export function Header() {
     return (
         <>
-
+            <Box
+                as='header'
+                w={'100%'}
+                height={'5vh'}
+            >
+                <nav>
+                    <UnorderedList
+                        fontSize={{ base: '3xl', lg: '5xl' }}
+                        display={'flex'}
+                        justifyContent={'space-around'}
+                        listStyleType={'none'}
+                        bg={'blue'}
+                        color={'#fff'}
+                        margin={0}
+                    >
+                        <Link href={'/'}><ListItem>logo</ListItem></Link>
+                        <Link href={'/'}><ListItem>商品</ListItem></Link>
+                        <Link href={'/'}><ListItem>お問い合わせ</ListItem></Link>
+                        <Link href={'/'}><ListItem>ご質問</ListItem></Link>
+                    </UnorderedList>
+                </nav>
+            </Box>
         </>
     )
 }
--- src/pages/samples/14/index.tsx
+++ src/pages/samples/14/index.tsx
@@ -1,12 +1,13 @@
 import { Box, Text, Flex, Input } from '@chakra-ui/react'
 import { useState } from 'react'
 import { motion } from 'framer-motion'
+import { Header } from './components/Header'
 
 export default function Sample14() {
     return (
         <>
             <Box>
-
+                <Header />
             </Box>
         </>
     )
